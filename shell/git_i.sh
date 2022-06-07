ed931d8907dd2f94b46b4614323c4a68ab583302
zhuzhenjie <mimotronik@gmail.com>
2022-06-07 15:40:55 +0800

    feat: shell
    install git

---
+++ shell/git_i.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+wget https://dl.ipcrystal.com/git/git-2.36.1.zip
+
+mv git-2.36.1.zip /usr/local
+
+cd /usr/local
+
+yum install -y curl-devel expat-devel gettext-devel openssl-devel zlib-devel unzip
+
+yum remove -y git
+
+unzip git-2.36.1.zip
+
+cd git-2.36.1
+
+make prefix=/usr/local/git all
+
+make prefix=/usr/local/git install
+
+echo "export PATH=\$PATH:/usr/local/git/bin" >> /etc/profile
+
+source /etc/profile
