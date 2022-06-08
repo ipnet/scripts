b25b3a25191d37ce6f0ff67f74ceed2e1d499214
zhuzhenjie <mimotronik@gmail.com>
2022-06-08 13:40:28 +0800

    feat: ipcrystal.sh folder

--- shell/ipcrystal.sh
+++ shell/ipcrystal.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-/bin/cp -rf msyql_i.sh /opt/download/mysql_i
+/bin/cp -rf msyql_i.sh /opt/download/quickstart/mysql_i
 
-/bin/cp -rf redis_i.sh /opt/download/redis_i
+/bin/cp -rf redis_i.sh /opt/download/quickstart/redis_i
 
-/bin/cp -rf git_i.sh /opt/download/git_i
+/bin/cp -rf git_i.sh /opt/download/quickstart/git_i
 
