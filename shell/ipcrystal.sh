d20dcb2a4b96b4fc4e38495cc90446c86e14c837
zhuzhenjie <mimotronik@gmail.com>
2022-06-08 14:36:47 +0800

    fix: ipcrystal.sh

--- shell/ipcrystal.sh
+++ shell/ipcrystal.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-
 /bin/cp -rf msyql_i.sh /opt/download/quickstart/mysql_i
-
 /bin/cp -rf redis_i.sh /opt/download/quickstart/redis_i
-
 /bin/cp -rf git_i.sh /opt/download/quickstart/git_i
-
 /bin/cp -rf jdkmvn_i.sh /opt/download/quickstart/jdkmvn_i
